d4b9acc01eb82958f54f04a6ccadbc18f9ed3dbd
adamijak <adamik.jakub72@gmail.com>
2022-01-12 01:26:18 +0100

    backup

---
+++ WebApp/Controllers/Device/DeviceSelectController.cs
@@ -0,0 +1,21 @@
+using Gas.Common.Models.Device;
+using Gas.Services.Cosmos;
+using Gas.Services.Devices;
+using Gas.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Gas.WebApp.Controllers;
+
+[Route("Device/Select")]
+public class DeviceSelectController : BaseController
+{
+    public DeviceSelectController(ILogger<DeviceSelectController> logger, ICosmosService dbService, IDeviceService deviceService) : base(logger, dbService, deviceService) { }
+
+    [HttpGet]
+    public async Task<IActionResult> IndexAsync()
+    {
+        var model = await NewBaseModel<BaseModel>("pepa", null);
+        return View(model);
+    }
+}
