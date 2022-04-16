33c14af76573ea251e8d6b9219bf4a30ddb9a562
adamijak <adamik.jakub72@gmail.com>
2022-04-16 02:15:28 +0200

    feat: code cleanup

--- Common/Extensions/ConfigExtensions.cs
+++ Common/Extensions/ConfigExtensions.cs
@@ -5,6 +5,6 @@ namespace Gas.Common.Extensions;
 public static class ConfigExtensions
 {
     public static string GetDatabaseId(this IConfiguration config) => config.GetValue<string>("DatabaseName");
-    public static string GetUsersContainerId(this IConfiguration config) => "users";
+    public static string GetUsersContainerId(this IConfiguration config) => config.GetValue<string>("UsersContainer");
     public static string GetReportContainerId(this IConfiguration config) => config.GetValue<string>("ReportsContainer");
 }
--- WebApp/Controllers/DeviceController.cs
+++ WebApp/Controllers/DeviceController.cs
@@ -1,30 +1,61 @@
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Linq;
 using System.Text.Json;
+using Adamijak.Azure.Cosmos.Extensions;
+using Gas.Common.Extensions;
+using Gas.Common.Items;
 using Gas.Common.Models.Device;
-using Gas.Common.Static;
 using Gas.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Devices;
 
 namespace Gas.WebApp.Controllers;
 
-[Route("Device/{deviceId}/Schedule/{scheduleId:int}")]
-public class DeviceScheduleController : BaseController
+[Route("[controller]")]
+public class DeviceController : BaseController
 {
     private readonly RegistryManager registryManager;
-    public DeviceScheduleController(ILogger<DeviceScheduleController> logger, IConfiguration config, CosmosClient cosmosClient, RegistryManager registryManager) : base(logger, config, cosmosClient)
+    private readonly Container reportContainer;
+
+    public DeviceController(ILogger<DeviceController> logger, IConfiguration config, CosmosClient cosmosClient, RegistryManager registryManager) : base(logger, config, cosmosClient)
     {
         this.registryManager = registryManager;
+        reportContainer = cosmosClient.GetContainer(config.GetDatabaseId(), config.GetReportContainerId());
+    }
+
+    [HttpGet("[action]")]
+    public async Task<IActionResult> SelectAsync()
+    {
+        var model = new BaseModel();
+        await FillBaseModel(model, null);
+        return View(model);
+    }
+
+    [HttpGet("{deviceId}/[action]")]
+    public async Task<IActionResult> StatusAsync(string deviceId)
+    {
+        var model = new StatusModel();
+        await FillBaseModel(model, deviceId);
+
+        if (model?.UserDevices == null || !model.UserDevices.Select(d => d.Id).Contains(deviceId))
+        {
+            return RedirectToAction("Index", "DeviceSelect");
+        }
+
+        var query = new QueryDefinition("SELECT * FROM c where c.deviceId = @deviceId and c.dateTime > @date")
+            .WithParameter("@deviceId", deviceId)
+            .WithParameter("@date", DateTime.UtcNow.AddDays(-7));
+
+        var reports = await reportContainer.GetItemsAsync<DeviceReport>(query);
+
+        model.SystemTemperatures = reports.Select(r => (r.DateTime, (r.SystemTemperature0 + r.SystemTemperature1) / 2)).ToList();
+        model.Rssis = reports.Select(r => (r.DateTime, r.Rssi)).ToList();
+
+        return View(model);
     }
 
 
-    [HttpGet]
-    public async Task<IActionResult> IndexAsync(string deviceId, int scheduleId)
+    [HttpGet("{deviceId}/[action]/{scheduleId:int}")]
+    public async Task<IActionResult> ScheduleAsync(string deviceId, int scheduleId)
     {
         var model = new ScheduleModel();
         await FillBaseModel(model, deviceId);
@@ -61,8 +92,8 @@ public class DeviceScheduleController : BaseController
     }
 
 
-    [HttpPost]
-    public async Task<IActionResult> IndexAsync(string deviceId, int scheduleId, ScheduleModel model)
+    [HttpPost("{deviceId}/[action]/{scheduleId:int}")]
+    public async Task<IActionResult> ScheduleAsync(string deviceId, int scheduleId, ScheduleModel model)
     {
         await FillBaseModel(model, deviceId);
         if (!ModelState.IsValid)
@@ -103,6 +134,6 @@ public class DeviceScheduleController : BaseController
         var twinPatch = @"{ properties : { desired : { schedules : " + json + "}}}";
 
         await registryManager.UpdateTwinAsync(deviceId, twinPatch, twin.ETag);
-        return RedirectToAction("Index");
+        return RedirectToAction("Schedule");
     }
 }
